b971b483260aef6d82379c740efb05e668b10a44
mnz <muneeskarakkad62@gmail.com>
2023-07-20 17:55:33 +0530

    dashboard stats data implimented from api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,57 @@
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import FeaturedInfo from "../components/FeaturedInfo";
 import Chart from "../components/Chart";
 import { data } from "../dummyData";
 import WidgetSm from "../components/WidgetSm";
 import WidgetLg from "../components/WidgetLg";
+import { userRequest } from "../requestmethods";
 
 const Home = () => {
+  const [userStats, setUserStats] = useState([]);
+  const MONTHS = useMemo(
+    () => [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+    []
+  );
+
+  useEffect(() => {
+    const getStats = async () => {
+      try {
+        const res = await userRequest.get("/user/stats");
+        // console.log("stats",res.data);
+        const updatedStats = res.data.map((item) => ({
+          name: MONTHS[item._id - 1],
+          "Active User": item.total,
+        }));
+
+        setUserStats(updatedStats)
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getStats();   
+  }, [MONTHS]);   
   return (
     <div className="flex-[4]">
       <FeaturedInfo />
-      <Chart title={"Sales Analytics"} data={data} dataKey={"User"} grid />
+      <Chart
+        title={"User Analytics"}
+        data={userStats}
+        dataKey={"Active User"}
+        grid
+      />
       <div className="flex m-5">
         <WidgetSm />
         <WidgetLg />
